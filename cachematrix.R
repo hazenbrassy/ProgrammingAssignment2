af2b9bc6103a03ede6cd22219e13eaef50329bdf
hazenbrassy <hazenbrassy@gmail.com>
2014-08-24 08:24:25 -0700

    First commit of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    
+    set <- function(solve) { 
+      x <<- solve
+      m <<- NULL      
+    }
+    get <- function() {x} ##original value of matrix    
+    setMatrix <- function(m) {m <<- solve(m)}
+    getMatrix <- function() {m} 
+    
+    list(set = set, get = get, setMatrix = setMatrix, getMatrix = getMatrix)
+}
 
+## Returns a matrix that is the inverse 
+cacheSolve <- function(x,...) {
+  m <- x$getMatrix()
+  
+  if(!is.null(m)) {
+    message("Getting Cached Data")
+    return(m) 
+  }
+  
+  data <- x$get()
+  m <- solve(data)
+  x$setMatrix(m)
+  m
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
